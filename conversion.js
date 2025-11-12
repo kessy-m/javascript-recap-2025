93da506ec1375f58a22d57a833d2ecd22eac4dbb
kessy-m <kessymuriithi@gmail.com>
2025-11-12 20:49:45 +0300

    type conversion added

---
+++ conversion.js
@@ -0,0 +1,26 @@
+// converting string to  number
+let num = '24.8'
+
+let x = Number(num)
+let y = parseFloat(num)
+let z = parseInt(num)
+
+console.log(num, typeof (num))
+console.log(x, typeof (x))
+console.log(y, typeof (y))
+console.log(z, typeof (z))
+
+// converting number to string
+let a = 104
+let b = String(a)
+let c = a.toString()
+
+console.log(a, typeof (a))
+console.log(b, typeof (b))
+console.log(c, typeof (c))
+
+
+let d='5'
+let f='12'
+let ct=d*f
+console.log(ct)
