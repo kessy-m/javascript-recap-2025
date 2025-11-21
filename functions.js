979166dc891e229d3813f9d6be3e7af8b6a7de1c
kessy-m <kessymuriithi@gmail.com>
2025-11-21 20:35:53 +0300

    functions in javascript

---
+++ functions.js
@@ -0,0 +1,29 @@
+let a = 12
+let b = 10
+let c = a + b
+console.log(c)
+
+function add_numbers() {
+    let a = 12
+    let b = 10
+    return a + b
+}
+sum = add_numbers()
+console.log(sum)
+
+// using parameters n arguments
+function sum_numbers(s, d) {
+    return s + d
+}
+sum1 = sum_numbers(10, 12)
+sum2 = sum_numbers(34, 89)
+
+console.log(sum1)
+console.log(sum2)
+
+// write a program that takes user input of a random number and checks 
+// if the number is even
+
+
+
+
